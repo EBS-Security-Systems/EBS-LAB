c79a0e43a010021f700530b8ddb3f159aa8d66f0
thiagosousa81 <thiagosousa81@gmail.com>
2025-01-03 11:27:24 -0300

    §

--- EBS-LAB/frmLogin.cs
+++ EBS-LAB/frmLogin.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Diagnostics;
+using System.Net.Http;
 using System.Drawing;
 using System.Linq;
 using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
 
 namespace EBS_LAB
 {
@@ -49,10 +51,54 @@ namespace EBS_LAB
             Process.Start(new ProcessStartInfo(@"https://web.ebs-systems.epizy.com/singup") { UseShellExecute = true });
         }
 
-        private void btnLogin_Click(object sender, EventArgs e)
+        private async void btnLogin_Click(object sender, EventArgs e)
         {
             string user = txtUser.Text;
             string pass = txtPassword.Text;
+
+            // URL do seu script PHP
+            string url = "https://web.ebs-systems.epizy.com/login/login.php";
+
+            using (HttpClient client = new HttpClient())
+            {
+                var postData = new StringContent(
+                    $"user={user}&pwd={pass}",
+                    Encoding.UTF8,
+                    "application/x-www-form-urlencoded"
+                );
+
+                try
+                {
+                    // Envia a requisição POST
+                    HttpResponseMessage response = await client.PostAsync(url, postData);
+
+                    // Lê a resposta da API
+                    string responseContent = await response.Content.ReadAsStringAsync();
+
+                    // Se a resposta for de sucesso, fazer algo (exibir a resposta ou redirecionar)
+                    if (response.IsSuccessStatusCode)
+                    {
+                        // Aqui, você pode verificar o status da resposta JSON
+                        if (responseContent.Contains("success"))
+                        {
+                            MessageBox.Show("Login bem-sucedido!");
+                        }
+                        else
+                        {
+                            MessageBox.Show("Erro: " + responseContent);
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Erro ao conectar com a API");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Erro: " + ex.Message);
+                }
+            }
         }
+    
     }
 }
